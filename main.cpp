34b80ac64c8340f867459693572b6a454adec722
katkadur <kd@mozog.com>
2016-10-09 14:15:28 -0500

    open this one for main (not compiled version)

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <unistd.h>
+
+
+int countdown() {
+
+    float numSeconds;
+    float* pointer = &numSeconds;
+    float pointedTo= *pointer;
+    std::cout << "Welcome to my countdown stopwatch timer!" << std::endl;
+    std::cout << "How many seconds to count down from??" << std::endl;
+    std::cin >> pointedTo;
+    std::cout << "Starting to count down from " << pointedTo << " seconds:" << std::endl;
+    for(pointedTo; pointedTo > 0; pointedTo--) {
+        std::cout  << "Time left: " << pointedTo << "\r" << std::flush;
+        sleep(1);
+
+    }
+
+    std::cout << std::endl << "TIMES UP!!!" << std::endl;
+
+
+
+    return 0;
+
+}
+
+int main()
+{
+    countdown();
+    return 0;
+}
